39b33416740f3bd02268cc5905234f9708ff4bf6
subhadip041 <subhadipnandy5@gmail.com>
2026-01-19 19:33:09 +0530

    add cookie parser

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,15 @@
 import express from "express"
 import { dbConnection } from "./config/db.js"
 import v1Router from "./routes/v1/index.js"
+import cookieParser from 'cookie-parser'
 
 const app = express()
+app.use(express.json())
+app.use(cookieParser())
+
 dbConnection()
 
-app.use(express.json())
+
 app.get('/', (req, res) => {
     res.status(200).json({
         msg: "server is running"
