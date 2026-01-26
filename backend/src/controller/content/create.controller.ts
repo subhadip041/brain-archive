ae82bd3e45757011d122f3fea04513505874576b
subhadip041 <subhadipnandy5@gmail.com>
2026-01-26 17:12:34 +0530

    add create control controller

---
+++ backend/src/controller/content/create.controller.ts
@@ -0,0 +1,55 @@
+import type { Request, Response } from "express";
+import { createContentType } from "../../types/content.types.js";
+import { Content } from "../../models/content.js";
+import { Tags } from "../../models/tag.js";
+
+
+export const createContentController = async (req: Request, res: Response) => {
+
+    try {
+
+        const createContentPayload = req.body;
+        const userId = (req as any).userId
+        const createContentPayloadCheck = createContentType.safeParse(createContentPayload)
+
+        if (!createContentPayloadCheck.success) {
+            return res.status(400).json({
+                msg: "Bad Request"
+            })
+        }
+
+        const { link, type, title, tags } = req.body
+
+
+        const existingTags = await Tags.find({
+            title: { $in: tags }
+        })
+
+        const existingTagTitle = existingTags.map(tag => tag.title)
+
+        const newTags = tags.filter((tag: string) => !existingTagTitle.includes(tag)).map((tag: string) => ({ title: tag }))
+
+        const createTag = await Tags.insertMany(newTags)
+
+        const tagIds = [...existingTags.map(tag_id => tag_id._id), ...createTag.map(tag_id => tag_id._id)]
+
+        const createContent = await Content.create({
+            link: link,
+            type: type,
+            title: title,
+            userId: userId,
+            tags: tagIds
+        })
+        return res.status(200).json({
+            msg: "new content created",
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({
+            msg: "Server error"
+        })
+
+    }
+
+}
--- backend/src/models/content.ts
+++ backend/src/models/content.ts
@@ -1,27 +1,29 @@
 import mongoose from "mongoose";
 
 enum contentType {
-  IMAGE = 'image',
-  VIDEO = 'video',
-  YOUTUBE = 'youtube',
-  TWEET = 'tweet',
-  AUDIO =   'audio'
+    IMAGE = 'image',
+    VIDEO = 'video',
+    YOUTUBE = 'youtube',
+    TWEET = 'tweet',
+    AUDIO = 'audio'
 
-}
+} 
 
 const contentSchema = new mongoose.Schema({
-    link:{
+    link: {
         type: String,
-        required:true
+        required: true
     },
-    type:{
+    type: {
         type: String,
         enum: Object.values(contentType)
     },
-    userId:{type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    tags: {type: mongoose.Schema.Types.ObjectId, ref: 'Tags'}
+    title: {
+        type: String,
+    },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tags' }],
 
-})
-export const Content = mongoose.model('Content',contentSchema)
+}, { timestamps: true })
+export const Content = mongoose.model('Content', contentSchema)
 
- 
