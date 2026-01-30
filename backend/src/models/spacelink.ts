fec30ce90aac71a2f9cf4868d66c90ce6bc23cd9
subhadip041 <subhadipnandy5@gmail.com>
2026-01-30 11:40:53 +0530

    update spacelink model

--- backend/src/models/spacelink.ts
+++ backend/src/models/spacelink.ts
@@ -1,9 +1,12 @@
 import mongoose from "mongoose";
 
 const spaceLinkSchema = new mongoose.Schema({
-    hash: { type: String, required: true },
-    userId:{type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    share: Boolean
-})
+    spaceTitle: { type: String, required: true },
+    hash: { type: String, unique: true, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    contents: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Content',default: [] }],
+    sharedWith: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User',default: [] }],
+    isPublic: { type: Boolean, default: false }
+}, { timestamps: true })
 
-export const spaceLink = mongoose.model('spaceLink',spaceLinkSchema)
+export const spaceLink = mongoose.model('spaceLink', spaceLinkSchema)
