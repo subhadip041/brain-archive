451e2cd5c3b3e91710468ad195a3ac0785de6d81
subhadip041 <subhadip041@gmail.com>
2026-03-30 22:09:01 +0530

    cors added

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,21 @@
 import express from "express"
+import cors from "cors"
 import { dbConnection } from "./config/db.js"
 import v1Router from "./routes/v1/index.js"
 import cookieParser from 'cookie-parser'
 dbConnection()
 
 const app = express()
+
+const corsOptions = {
+  origin: 'http://localhost:8000', 
+  allowedHeaders: ['Content-Type', 'Authorization']
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(cookieParser())
+
 
 
 
